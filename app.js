128e30c9ccea0bee8447668ea5f9e8204d07938a
Numbers00 <kmaquino@up.edu.ph>
2023-08-09 14:51:15 +0800

    (render) Trigger working deployment for exer 11.12

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.get('/version', (req, res) => {
-  res.send('2')
+app.get('/health', (_req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (_req, res) => {
+  res.send('3')
 })
 
 app.listen(PORT, () => {
